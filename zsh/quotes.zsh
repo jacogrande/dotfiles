3b31187213b0e79da350f3329d22a64b31e14bf0
Jackson Prowell <prowelljackson@gmail.com>
2023-10-11 14:10:15 -0700

    feat(Quotes): Quote script and json file

---
+++ zsh/quotes.zsh
@@ -0,0 +1,68 @@
+QUOTES_FILE="$DOTFILE_PATH/quotes.json" 
+
+sanitize_quote_input() {
+  echo "$1" | sed 's/"/\\"/g' | sed "s/'/\\'/g" | sed 's/\n/\\n/g' | sed 's/\r//g' | sed 's/\t/\\t/g'
+}
+
+quote() {
+  AUTHOR=$(sanitize_quote_input "$(gum input --placeholder "author")")
+  QUOTE=$(sanitize_quote_input "$(gum write --placeholder "quote")") 
+  SOURCE=$(sanitize_quote_input "$(gum input --placeholder "source")")
+
+
+
+  # Creating a JSON object
+  JSON_STRING=$(printf '{"source": "%s", "author": "%s", "quote": "%s"}' \
+    "$SOURCE" "$AUTHOR" "$QUOTE")
+
+
+  # Ensuring that the file exists
+  touch "$QUOTES_FILE"
+
+  # Ensuring that the JSON file is an array if it's empty
+  if [ ! -s "$QUOTES_FILE" ]; then
+    echo '[]' > "$QUOTES_FILE" 
+  fi
+
+  # Adding JSON object to the JSON file
+  gum format -- "$QUOTE" "" "**$AUTHOR**, *\"$SOURCE\"*"
+  gum confirm "Save this quote?" && jq ". += [$JSON_STRING]" "$QUOTES_FILE" > tmp.json && \mv tmp.json "$QUOTES_FILE"
+}
+
+get_random_quote() {
+  # Check if the file exists and is not empty
+  if [ ! -s "$QUOTES_FILE" ]; then
+    echo "No quotes available."
+    return 1
+  fi
+
+  # Get a random quote from the JSON file
+  # outputs each quote as a line, shuffles the lines, and outputs the first line
+  RANDOM_QUOTE=$(jq -c '.[] | select(.quote != null)' "$QUOTES_FILE" | sort -R | head -n 1)
+
+  # Check if jq or sort commands failed
+  if [ $? -ne 0 ]; then
+    echo "Error fetching quote."
+    return 1
+  fi
+  
+  # Extract fields from the JSON object
+  AUTHOR=$(echo "$RANDOM_QUOTE" | jq -r '.author')
+  QUOTE=$(echo "$RANDOM_QUOTE" | jq -r '.quote')
+  SOURCE=$(echo "$RANDOM_QUOTE" | jq -r '.source')
+
+  # check the length of source
+  if [ ${#SOURCE} -gt 0 ]; then
+    SOURCE=", *\"$SOURCE\"*"
+  fi
+
+  if [ $? -ne 0 ]; then
+    echo "Error extracting quote information."
+    return 1
+  fi
+
+  FORMATTED_QUOTE=$(gum format -- "$QUOTE" "" "**$AUTHOR**$SOURCE" "")
+
+  gum style --width 80 --align left "$FORMATTED_QUOTE"
+
+}
